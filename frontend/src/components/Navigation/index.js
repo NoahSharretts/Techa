98941ede927e49cad06855df02c6734c6e55ed50
Noah <garciasharrettsnoah@gmail.com>
2022-09-22 16:11:01 -0700

    attempting to implement seacrh bar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,31 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ProfileButton from './ProfileButton';
-
 import CreatePostModal from '../CreatePostModal';
+
+
 import './Navigation.css';
 
 
 function Navigation({ isLoaded }){
+  const dispatch = useDispatch()
   const sessionUser = useSelector(state => state.session.user);
+  const [input, setInput] = useState("");
 
-  // let sessionLinks;
-  // if (sessionUser) {
-  //   sessionLinks = (
-  //     <>
-
-  //     </>
-  //   );
-  // }
 
   return (
     <>
       {sessionUser && (
-        <div className='navBar'>
-          <NavLink to="/">Home</NavLink>
-          <CreatePostModal />
-          <ProfileButton user={sessionUser} />
+        <div className='nav-wrapper'>
+          <div className='nav'>
+            <div className='logo-wrapper'>
+              <NavLink to="/">
+                <img className='logo' src='https://i.imgur.com/KrRwLMe.png' />
+              </NavLink>
+            </div>
+            <div className='search-bar'>
+              <div className='search-container'>
+                <input
+                  className='search-bar-input'
+                  value={input}
+                  placeholder='Search'
+                  onFocus={() => showSearch()}
+                  onChange={(e) => setInput(e.target.value)}
+                />
+                <div className='search-results'>
+
+                </div>
+              </div>
+            </div>
+            <CreatePostModal />
+            <ProfileButton user={sessionUser} />
+          </div>
         </div>
       )}
     </>
